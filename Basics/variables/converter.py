ae978c8aba6156b0908d69ada099d79f5c05983c
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-03-26 11:45:38 +0530

    converter.py
    Example of a converter to convert Celsius to Fahrenheit and vice versa

---
+++ Basics/variables/converter.py
@@ -0,0 +1,8 @@
+celcius = 12
+fahrenhiet = 2
+
+fahrenhiet_to_celcius = (fahrenhiet - 32) * 5/9 ###Fahrenhiet to Celcius formula (32°F − 32) × 5/9 = 0°C
+celcius_to_fahrenhiet = (celcius * 9/5) + 32 ###celsius to fahrenhiet formula (0°C × 9/5) + 32 = 32°F
+
+print(celcius, "* celcius to fahrehiet = ", fahrenhiet_to_celcius)
+print(fahrenhiet, "* fahrenhiet to celcius = ", celcius_to_fahrenhiet)
