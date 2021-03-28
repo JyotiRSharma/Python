f8daf9b2d5674e8080eef3cfef6fad21bcc80b5b
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-03-28 20:09:14 +0530

    prettyVowelEater.py
    vowel eater upgraded. Storing the output in a variable.

--- Basics/LoopControl/vowelEater.py
+++ Basics/LoopControl/vowelEater.py
@@ -1,9 +1,11 @@
+word_without_vowels = ""
+
 # Prompt the user to enter a word
 # and assign it to the user_word variable.
 user_word = input("Enter a word")
 
 for letter in user_word.upper():
-    # Complete the body of the for loop.
+    # Complete the body of the loop.
     if letter == "A":
         continue
     elif letter == "E":
@@ -16,5 +18,7 @@ for letter in user_word.upper():
         continue # continue skips to the condition part of the loop
     
     else:
-        print(letter)
-        
+        word_without_vowels += letter
+
+# Print the word assigned to word_without_vowels.
+print(word_without_vowels)
