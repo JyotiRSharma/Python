8eda37844e7596282f60571f4e1960ca7215244c
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-04-19 08:37:17 +0530

    Create ImageScapper.py
    Get an image and add it to the path where the python program is located

---
+++ TheBeginning/ImageScapper.py
@@ -0,0 +1,10 @@
+import random
+import urllib.request
+
+def download_web_image(url):
+    name = random.randrange(1, 1000)
+    final_name = str(name) + '.jpg'
+
+    urllib.request.urlretrieve(url, final_name)
+
+download_web_image("https://drive.google.com/file/d/0BwG-9IEc88tdVUt1X1NmaC13SVU/view?usp=sharing")
