8065783893655b06ab2906ec595bfc2ee332b2e5
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-04-25 13:23:04 +0530

    Create binary_operations.py
    binary and, or, right shift and left shift operations

---
+++ TheBeginning/Bitwise_Operations/binary_operations.py
@@ -0,0 +1,14 @@
+a = 50    # 110010
+b = 20    # 011001
+
+c = a & b  # binary and
+print(c)
+
+d = a | b  # binary or
+print(d)
+
+# Right shift 2 steps
+print(a >> 2)  # 001100
+
+# Left shift 2 steps
+print(c << 2)  # 001000
