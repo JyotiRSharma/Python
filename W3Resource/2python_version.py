1c0f828dfdcae08698d8671c3767c541f7bbb34d
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-05-08 23:11:04 +0530

    Show the installed python version

---
+++ W3Resource/2python_version.py
@@ -0,0 +1,7 @@
+import sys
+
+print("Python version: ")
+print(sys.version)
+print("Python version info: ")
+print(sys.version_info)
+# print(" Major = ", sys.version_info.major, "\n Minor = ", sys.version_info.minor, "\n Micro = ", sys.version_info.micro)
